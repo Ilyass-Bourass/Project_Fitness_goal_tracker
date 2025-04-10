fc5483b05dbc600be8e82a6151c6c156e39bfc42
ilyass bourass <ilyass.b.1992@gmail.com>
2025-04-10 15:20:47 +0100

    Ajout de la fonctionnalité pour créer un nouvel objectif

--- src/components/GoalForm.jsx
+++ src/components/GoalForm.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function GoalForm() {
     
+  const [Goal,setGoal]=useState({
+    title:'',
+    description:'',
+    category: 'steps',
+    unit:'pas'
+  });
+
+  const [goalsList,setGoalsList]=useState(()=>{
+    const saved=localStorage.getItem('goals');
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const handelSubmit =(e)=>{
+    e.preventDefault();
+    console.log('avant',goalsList);
+    const updatedGoals = [...goalsList, Goal];
+    console.log(updatedGoals);
+
+    localStorage.setItem('goals', JSON.stringify(updatedGoals));
+
+  }
+
+  const hadelChaange =(e)=>{
+    const {name,value}=e.target;
+    setGoal({...Goal,[name]:value});
+  }
 
     return (
     <div className="goal-form">
       <h3>Ajouter un nouvel objectif</h3>
-      <form >
+      <form onSubmit={handelSubmit}>
         <div className="form-group">
           <label>
             Titre:
             <input
               type="text"
               name="title"
-              
+              onChange={hadelChaange}
+              value={Goal.title}
               placeholder="Ex: Marcher 10 000 pas"
               required
             />
@@ -25,6 +52,8 @@ export default function GoalForm() {
             Description:
             <textarea
               name="description"
+              value={Goal.description}
+              onChange={hadelChaange}
               placeholder="Description de votre objectif"
             />
           </label>
@@ -33,7 +62,7 @@ export default function GoalForm() {
         <div className="form-group">
           <label>
             Catégorie:
-            <select name="category" >
+            <select name="category" onChange={hadelChaange} value={goalsList.category}>
               <option value="steps">Pas</option>
               <option value="workout">Entraînement</option>
               <option value="water">Eau</option>
@@ -49,18 +78,22 @@ export default function GoalForm() {
                 type="number"
                 name="target"
                 placeholder="Valeur cible"
+                value={Goal.target}
+                onChange={hadelChaange}
                 required
               />
               <input
                 type="text"
                 name="unit"
+                value={Goal.unit}
+                onChange={hadelChaange}
                 placeholder="Unité"
               />
             </div>
           </label>
         </div>
         
-        <button type="submit">Ajouter objectif</button>
+        <button type='submit'>Ajouter objectif</button>
       </form>
     </div>
   );
